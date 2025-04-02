5e636db0deda91ed6abbb070635cad349d7bfc42
2200090049 <2200090049@kluniversity.in>
2025-04-02 12:11:17 +0530

    Updated MySQL connection for Railway

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.MYSQLHOST,
+    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   }
 );
