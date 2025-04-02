76e5970a738b9ee903dfcf848c6b5f69f9aa6478
2200090049 <2200090049@kluniversity.in>
2025-04-02 12:07:35 +0530

    Updated MySQL connection for Railway

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use("/api", userRoutes);
 // Setup Swagger
 setupSwagger(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.MYSQLPORT || 5000;
 
 app.listen(PORT, async () => {
   await sequelize.sync();
--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST,
     dialect: process.env.DB_DIALECT,
   }
 );
